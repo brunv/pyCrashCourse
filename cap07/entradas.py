c6fb717a7a924013e3b97dd20aa500606710d425
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2018-12-23 15:07:27 -0200

    cap07 init

---
+++ cap07/entradas.py
@@ -0,0 +1,10 @@
+#
+#       Aceitando entradas de usuário
+#
+
+idade_str = input("Quantos anos você tem? ")    # aceita entrada em string por padrão
+idade_int = input("Quantos ano você tem? ")
+idade_int = int(idade_int)                      # transforma idade em int
+
+print(idade_str)
+print(idade_int)
