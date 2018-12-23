018f70d2c407509bc91afb55b711853cc3897e8b
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2018-12-23 15:43:58 -0200

    flags

---
+++ cap07/flags.py
@@ -0,0 +1,16 @@
+#
+#       Utilizando flags para facilitar as verificações
+#
+
+
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
