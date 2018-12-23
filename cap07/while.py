ecd3e6a26b7c19ecb8092543275b7627cb16e35d
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2018-12-23 15:53:16 -0200

    mais laços while

--- cap07/while.py
+++ cap07/while.py
@@ -11,3 +11,20 @@ while message != 'quit':
     message = input(prompt)
     if message != 'quit':
         print(message)
+
+
+#
+#       Usando break para sair de um laço
+#
+
+
+prompt = "\nPlease enter the name of a city you have visited: "
+prompt += "\nEnter 'quit' when you are finished."
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I\'d love to go to " + city.title() + "!")
