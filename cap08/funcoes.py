03927a5941011e82a892c19e3301c8730d18bcb4
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2018-12-27 22:16:50 -0200

    cap08 funções init

---
+++ cap08/funcoes.py
@@ -0,0 +1,24 @@
+#       Para definirmos uma função em Python começamos utilizando a palavra reservada
+#       def, seguida do nome da função e dos parâmetros entre parênteses e terminada
+#       em dois pontos.
+
+
+def greetings():
+    """Exibe uma mensagem de saudação."""
+    print("Hello! Welcome!")
+
+greetings()
+
+
+#       Na linha 7, esse tipo de comentário é chamado docstring e descreve o que a
+#       função faz. Python procura as docstrings quando gera a documentação das
+#       funções do programa.
+
+
+#       Passando informações para uma função
+
+def greet_user(username):
+    """Exibe uma mensagem de saudação para um determinado usuário."""
+    print("Welcome, " + username.title() + "!")
+
+greet_user('johndoe')
