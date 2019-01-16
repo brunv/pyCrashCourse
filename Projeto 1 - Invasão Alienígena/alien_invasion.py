83ce44c76d0b6ff7b16144eaaf7d2093fb3e7c09
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-16 19:03:29 -0200

    refatorando

--- Projeto 1 - Invasão Alienígena/alien_invasion.py
+++ Projeto 1 - Invasão Alienígena/alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     """Inicializa o jogo e cria um objeto para a tela."""
@@ -18,38 +18,31 @@ def run_game():
     #   Inicia o laço principal do jogo
     while True:
 
-        # 	Observa eventos de teclado e de mouse
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()		#	Clicar no botão de saída
-        
-        # Redesenha a tela a cada passagem do laço
-        screen.fill(config.bg_color)
-        ship.blitme()
-
-        # Deixa a tela mais recente visível
-        pygame.display.flip()
+        #   Observa eventos de teclado e de mouse
+        gf.check_events()
+        #   Resenha a tela a cada passagem do laço
+        gf.update_screen(config, screen, ship)
 
 run_game()
 
 
-#		O módulo 'pygame' contém as funcionalidades necessárias para criar um jogo.
-#		O módulo 'sys' será utilizado para sair do jogo quando o usuário quiser.
+#	    O módulo 'pygame' contém as funcionalidades necessárias para criar um jogo.
+#	    O módulo 'sys' será utilizado para sair do jogo quando o usuário quiser.
 #
 #   	O método init() inicializa as configurações de segundo plano que o Pygame
-#		precisa para funcionar de maneira apropriada.
+#	    precisa para funcionar de maneira apropriada.
 #
-#		Instanciamos Settings() em config após o início de pygame.init()
+#	    Instanciamos Settings() em config após o início de pygame.init()
 #
-#		Chamamos display.set_mode() para criar uma janela de exibição na qual
-#		desenharemos todos os elementos gráficos do jogo. O argumento passado
-# 		define as dimensões da janela do jogo.
+#	    Chamamos display.set_mode() para criar uma janela de exibição na qual
+#	    desenharemos todos os elementos gráficos do jogo. O argumento passado
+# 	    define as dimensões da janela do jogo.
 #
-#		Chamamos display.set_caption() para dar nome à janela do jogo.
+#	    Chamamos display.set_caption() para dar nome à janela do jogo.
 #
-# 		Para fazer nosso programa responder aos eventos, escreveremos um laço
-#		de eventos apara ouvir um evento e executar uma tarefa apropriada de
-#		acordo com o tipo de evento ocorrido utilizando event.get().
+# 	    Para fazer nosso programa responder aos eventos, escreveremos um laço
+#	    de eventos apara ouvir um evento e executar uma tarefa apropriada de
+#	    acordo com o tipo de evento ocorrido utilizando event.get().
 #
-#		Desenhamos a espaçonave na tela chamando 'ship.blitme()' depois de
-#		preencher a cor de fundo; assim a espaçonave aparecerá sobre essa cor.
+#	    Desenhamos a espaçonave na tela chamando 'ship.blitme()' depois de
+#	    preencher a cor de fundo; assim a espaçonave aparecerá sobre essa cor.
--- Projeto 1 - Invasão Alienígena/game_functions.py
+++ Projeto 1 - Invasão Alienígena/game_functions.py
@@ -1,9 +1,19 @@
-import symbol
+import sys
 import pygame
 
 def check_events():
     """Responde a eventos de pressionamento de teclas e de mouse."""
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        if event.type == pygame.QUIT:   # clique no botão de fechar janela
+            sys.exit()                  # fecha o programa
+
+def update_screen(config, screen, ship):
+    """Atualiza as imagens na tela e altera para a nova tela."""
+
+    # Redesenha a tela a cada passagem do laço
+    screen.fill(config.bg_color)
+    ship.blitme()
+
+    # Deixa a tela mais recente visível
+    pygame.display.flip()
