385532ad8cd9953230c5dca7b93e662576d89991
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-16 20:59:45 -0200

    adiciona projéteis

---
+++ Projeto 1 - Invasão Alienígena/bullets.py
@@ -0,0 +1,37 @@
+import pygame
+from pygame.Sprite import Sprite
+
+class Bullet(Sprite):
+    """Uma classe que administra projéteis disparados pela espaçonave."""
+
+    def __init__(self, config, screen, ship):
+        """Cria um objeto para o projétil na posição atual da espaçonave."""
+
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        #   Cria um retângulo para o projétil em (0,0) e, em seguida, define a
+        #   posição correta.
+        self.bullet_rect = pygame.Rect(0, 0, config.bullet_width, 
+            config.bullet_heigth)
+        self.bullet_rect.centerx = ship.ship_rect.centerx
+        self.bullet_rect.top = ship.ship_rect.top
+
+        #   Armazena a posição do projétil como um valor decimal
+        self.bullet_y = float(self.bullet_rect.y)
+
+        self.bullet_color = config.bullet_color
+        self.bullet_speed_factor = config.bullet_speed_factor
+
+    def update(self):
+        """Move o projétil para cima na tela."""
+
+        #   Atualiza a posição decimal do projétil
+        self.bullet_y -= self.bullet_speed_factor
+        #   Atualiza a posição de rect
+        self.bullet_rect = self.bullet_y
+
+    def draw_bullet(self):
+        """Desenha o projétil na tela."""
+
+        pygame.draw.rect(self.screen, self.bullet_color, self.bullet_rect)
--- Projeto 1 - Invasão Alienígena/settings.py
+++ Projeto 1 - Invasão Alienígena/settings.py
@@ -10,5 +10,10 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         #   Configurações da espaçonave
-
         self.ship_speed_factor = 1.5
+
+        #   Configurações dos projéteis
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
