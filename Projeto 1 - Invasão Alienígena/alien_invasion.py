457abaa7ed6b6e97eb895151292a214f7c072b73
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-16 17:16:21 -0200

    reorganiza alien_invasion.py

--- Projeto 1 - Invasão Alienígena/alien_invasion.py
+++ Projeto 1 - Invasão Alienígena/alien_invasion.py
@@ -1,30 +1,20 @@
-#	O módulo 'pygame' contém as funcionalidades necessárias para criar um jogo.
-#	O módulo 'sys' será utilizado para sair do jogo quando o usuário quiser.
 import sys
 import pygame
 from settings import Settings
 
 def run_game():
-    #   O método init() inicializa as configurações de segundo plano que o Pygame
-	#	precisa para funcionar de maneira apropriada.
+	"""Inicializa o jogo e cria um objeto para a tela."""
+
+	#	Inicializa o jogo
     pygame.init()
-	#	Instancia a classe de configuração (depois da chamada de pygame.init())
     config = Settings()
-	#	Chamamos display.set_mode() para criar uma janela de exibição na qual
-	#	desenharemos todos os elementos gráficos do jogo.
-	#	O argumento passado define as dimensões da janela do jogo.
-    screen = pygame.display.set_mode(
-            (config.screen_width, config.screen_height)
-    )
-	#	Chamamos display.set_caption() para dar nome à janela do jogo.
+    screen = pygame.display.set_mode((config.screen_width, config.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #   Inicia o laço principal do jogo
     while True:
-
-        # 	Para fazer nosso programa responder aos eventos, escreveremos um laço
-		#	de eventos apara ouvir um evento e executar uma tarefa apropriada de
-		#	acordo com o tipo de evento ocorrido utilizando event.get().
+		
+		# 	Observa eventos de teclado e de mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()		#	Clicar no botão de saída
@@ -35,5 +25,23 @@ def run_game():
         # Deixa a tela mais recente visível
         pygame.display.flip()
 
-
 run_game()
+
+
+#		O módulo 'pygame' contém as funcionalidades necessárias para criar um jogo.
+#		O módulo 'sys' será utilizado para sair do jogo quando o usuário quiser.
+#
+#   	O método init() inicializa as configurações de segundo plano que o Pygame
+#		precisa para funcionar de maneira apropriada.
+#
+#		Instanciamos Settings() em config após o início de pygame.init()
+#
+#		Chamamos display.set_mode() para criar uma janela de exibição na qual
+#		desenharemos todos os elementos gráficos do jogo. O argumento passado
+# 		define as dimensões da janela do jogo.
+#
+#		Chamamos display.set_caption() para dar nome à janela do jogo.
+#
+# 		Para fazer nosso programa responder aos eventos, escreveremos um laço
+#		de eventos apara ouvir um evento e executar uma tarefa apropriada de
+#		acordo com o tipo de evento ocorrido utilizando event.get().
