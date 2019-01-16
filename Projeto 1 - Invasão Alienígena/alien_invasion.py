a4d4fb53424eb35e00bffb2002db9c946ee00a6e
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-16 16:14:52 -0200

    Update alien_invasion.py

--- Projeto 1 - Invasão Alienígena/alien_invasion.py
+++ Projeto 1 - Invasão Alienígena/alien_invasion.py
@@ -1,26 +1,33 @@
+#	O módulo 'pygame' contém as funcionalidades necessárias para criar um jogo.
+#	O módulo 'sys' será utilizado para sair do jogo quando o usuário quiser.
 import sys
 import pygame
 from settings import Settings
 
 def run_game():
-    #   Inicializa o jogo e cria um objeto para a tela
+    #   O método init() inicializa as configurações de segundo plano que o Pygame
+	#	precisa para funcionar de maneira apropriada.
     pygame.init()
+	#	Instancia a classe de configuração (depois da chamada de pygame.init())
     config = Settings()
+	#	Chamamos display.set_mode() para criar uma janela de exibição na qual
+	#	desenharemos todos os elementos gráficos do jogo.
+	#	O argumento passado define as dimensões da janela do jogo.
     screen = pygame.display.set_mode(
             (config.screen_width, config.screen_height)
     )
+	#	Chamamos display.set_caption() para dar nome à janela do jogo.
     pygame.display.set_caption("Alien Invasion")
-
-    #   Define a cor de fundo
-    bg_color = (230, 230, 230)
 
     #   Inicia o laço principal do jogo
     while True:
 
-        # Observa eventos de teclado e mouse
+        # 	Para fazer nosso programa responder aos eventos, escreveremos um laço
+		#	de eventos apara ouvir um evento e executar uma tarefa apropriada de
+		#	acordo com o tipo de evento ocorrido utilizando event.get().
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                sys.exit()		#	Clicar no botão de saída
 
         # Redesenha a tela a cada passagem do laço
         screen.fill(config.bg_color)
