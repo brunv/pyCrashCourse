38c7915e8a399ad94ab9d6564d8a82602796e8e5
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-24 21:44:58 -0200

    plota dados da população mundial no mapa

--- Projeto 2 - Visualização de Dados/16 - Fazendo Download de Dados/populacao_mundial.py
+++ Projeto 2 - Visualização de Dados/16 - Fazendo Download de Dados/populacao_mundial.py
@@ -3,6 +3,7 @@
 #
 
 import json
+from pygal.maps.world import World 
 from country_codes import get_country_code
 
 #   Carrega dados em uma lista
@@ -10,7 +11,9 @@ filename = 'data/population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-#   Exibe a população de cada país em 2010
+#   Constrói um dicionário com dados das populações
+cc_populations = {}
+
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -18,10 +21,30 @@ for pop_dict in pop_data:
         code = get_country_code(country_name)
 
         if code:
-            print(code + ": " + str(population))
-        else:
-            print("ERROR: " + country_name)
+            cc_populations[code] = population
 
+#   Agrupa os países em três níveis populacionais
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+#   Vê quantos países estão em cada nível
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+#   Cria o mapa
+wm = World()
+wm.title = 'World Populations in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('data/populacao_mundial.svg')
 
 
 #       A função 'json.load()' converte os dados em um formato com que Python possa
@@ -41,6 +64,9 @@ for pop_dict in pop_data:
 #       'i18n', que é uma abreviatura para internationalization. O dicionário
 #       COUNTRIES contém os códigos de duas letras dos países como chaves e os
 #       nomes dos países como valores.
+#
+#       NOTA: O dicionário COUNTRIES e o módulo worldmap foram movidos para outro
+#       pacote, o pygal_maps_world.
 #
 #       Os erros em relação aos nomes e códigos de países têm duas origens. Em 
 #       primeiro lugar, nem todas as classificações no conjunto de dados são
