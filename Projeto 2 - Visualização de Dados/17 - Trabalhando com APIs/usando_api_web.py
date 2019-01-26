79dccf1980f2fa1a9c4d0ffb7bdd94387249d72d
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-26 16:23:55 -0200

    adiciona descrição e link no gráfico

--- Projeto 2 - Visualização de Dados/17 - Trabalhando com APIs/usando_api_web.py
+++ Projeto 2 - Visualização de Dados/17 - Trabalhando com APIs/usando_api_web.py
@@ -74,11 +74,22 @@ print("Total repositories:", response_dict['total_count'])
 
 #   Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
+print('Number of items:', len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided."
+    
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
 #   Cria visualização
 my_style = LS('#333366', base_style=LCS)
@@ -97,7 +108,7 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('test', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
 
 
