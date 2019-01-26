30144519176aed02880c68f9ef3220aa06038e19
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-26 14:38:42 -0200

    analisa as chaves do objeto retornado

--- Projeto 2 - Visualização de Dados/17 - Trabalhando com APIs/usando_api_web.py
+++ Projeto 2 - Visualização de Dados/17 - Trabalhando com APIs/usando_api_web.py
@@ -57,13 +57,21 @@ import requests
 #   Faz uma chamada de API e armazena a resposta
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Status Code: " + str(r.status_code))
+print("Status Code:", r.status_code)
 
 #   Armazena a resposta da API em uma variável
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-#   Processa o resultado devolvendo as chaves
-print(response_dict.keys())
+#   Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+#   Analisa o primeiro repositório e exibe todas as suas chaves
+repo_dict = repo_dicts[0]
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
 
 
 #       Chamamos 'get()', passamos o URL e armazenamos o objeto com a resposta na
