974ef21d8668df37d4517608f453ad8069278346
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-29 18:08:24 -0200

    registra o modelo Topic na administração

--- Projeto 3 - Aplicações Web/learning_logs/admin.py
+++ Projeto 3 - Aplicações Web/learning_logs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from learning_logs.models import Topic
 
 # Register your models here.
+admin.site.register(Topic)
+
+
+#       Esse código importa o modelo que queremos registrar e então usa
+#       'admin.site.register()' para dizer a Django que administre nosso modelo por
+#       meio do site de administração.
