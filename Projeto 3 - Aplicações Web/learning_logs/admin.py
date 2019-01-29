2aac7e3d28333afcdb616ec19ac17ab0a41e0321
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-29 19:09:37 -0200

    registra Entry na administração

--- Projeto 3 - Aplicações Web/learning_logs/admin.py
+++ Projeto 3 - Aplicações Web/learning_logs/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from learning_logs.models import Topic
+from learning_logs.models import Topic, Entry
 
 # Register your models here.
 admin.site.register(Topic)
-
+admin.site.register(Entry)
 
 #       Esse código importa o modelo que queremos registrar e então usa
 #       'admin.site.register()' para dizer a Django que administre nosso modelo por
