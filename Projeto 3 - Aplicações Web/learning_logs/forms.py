0dcf3916099fc770fc8fbb2237da88e3fda7c6b2
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-02-03 14:37:31 -0200

    cria formulário para entradas

--- Projeto 3 - Aplicações Web/learning_logs/forms.py
+++ Projeto 3 - Aplicações Web/learning_logs/forms.py
@@ -18,3 +18,17 @@ class TopicForm(forms.ModelForm):
 #       Em 'Meta', criamos um formulário a partir do modelo 'Topic' e incluímos
 #       apenas o campo 'text'. Esse código diz a Django para não gerar um label
 #       para o campo 'text'.
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
+
+#       Um 'widget' é um elemento do formulário HTML, por exemplo, uma caixa de
+#       texto de uma única linha, uma área de texto com várias linhas ou uma lista
+#       suspensa. Ao incluir o atributo 'widgets', podemos sobrescrever opções
+#       default de widgets de Django.
