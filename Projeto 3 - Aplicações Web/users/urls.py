a99a6601009f1e4df89befa745b7c5c455db0bd9
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-02-03 17:10:57 -0200

    cria url para a aplicação users

---
+++ Projeto 3 - Aplicações Web/users/urls.py
@@ -0,0 +1,21 @@
+"""Define padrões de URL para users."""
+
+from django.conf.urls import url
+from django.contrib.auth.views import views as auth_views
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Página de login
+    path('login/',
+        auth_views.LoginView.as_view(template_name='users/login.html'),
+        name='login'),
+]
+
+
+#       No Django 2.0+, o 'namesapce' do app é definido pela variável 'app_name' e
+#       não há necessidade do uso de expressões regulares.
+#
+#       Como estamos usando uma view padrão de Django, a 'LoginView', a sintaxe
+#       fica um pouco diferente. Para usar um template nosso, passamos o argumento
+#       'template_name' com o caminho para tal template.
