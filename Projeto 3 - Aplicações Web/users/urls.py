359638951d2a7b1bf7edd04294d0765f2f0d3755
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-02-03 18:12:56 -0200

    adicionar url para logout

--- Projeto 3 - Aplicações Web/users/urls.py
+++ Projeto 3 - Aplicações Web/users/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('login/',
         auth_views.LoginView.as_view(template_name='users/login.html'),
         name='login'),
+
+    # Página de logout
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 
@@ -19,3 +22,6 @@ urlpatterns = [
 #       Como estamos usando uma view padrão de Django, a 'LoginView', a sintaxe
 #       fica um pouco diferente. Para usar um template nosso, passamos o argumento
 #       'template_name' com o caminho para tal template.
+#
+#       No URL de logout utilizamos a view 'LogoutView' default. Também não
+#       nenhum argumento de template pois não há template de logout.
